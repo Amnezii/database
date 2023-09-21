6f72270d6f0b0377f60534c55b285cc0350ec744
unknown <golubnichiyvladik@gmail.com>
2023-09-21 15:05:04 +0200

    sql3

---
+++ sql3.sql
@@ -0,0 +1,22 @@
+1. SELECT name
+FROM cities
+WHERE name like '%ськ%'
+
+2. SELECT name
+FROM cities
+WHERE name like '%донец%'
+
+3. SELECT CONCAT(name, ' (', region, ')') as назва_регіон
+FROM cities
+WHERE population > 100000
+order by name desc
+
+4. SELECT name, population, (population / 40000000.0) * 100 AS '%'
+FROM cities
+ORDER BY population DESC
+LIMIT 10;
+
+5. SELECT CONCAT(name, ' - ', ((population / 40000000.0) * 100), '%') AS 'name+%'
+FROM cities
+WHERE (population / 40000000.0) * 100 >= 0.1
+ORDER BY ((population / 40000000.0) * 100) ASC;
